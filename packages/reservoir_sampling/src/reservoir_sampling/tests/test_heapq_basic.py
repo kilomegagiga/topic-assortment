a60d9a609561ba88c2513b385428be49573e43b3
kilomegagiga <kilomegagiga93@gmail.com>
2026-08-10 04:44:12 -0500

    Additional functionality tests for heapq.

--- packages/reservoir_sampling/src/reservoir_sampling/tests/test_heapq_basic.py
+++ packages/reservoir_sampling/src/reservoir_sampling/tests/test_heapq_basic.py
@@ -14,3 +14,9 @@ class Test_heapq_module:
         heapq.heappush(pq, (self.oneSeventh, "a seventh"))
         assert self.oneSeventh == pq[0][0]
 
+    def test_mustReturnHeapCount(self):
+        pq = []
+        heapq.heappush(pq, (self.oneSeventh, "a seventh"))
+        heapq.heappush(pq, (self.oneTenth, "a tenth"))
+        heapq.heappush(pq, (self.oneFifth, "a fifth"))
+        assert 3 == len(pq)
