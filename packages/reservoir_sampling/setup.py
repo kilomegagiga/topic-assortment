81ea39b81cf8f1326caa71a1533acbbb98e31560
kilomegagiga <kilomegagiga93@gmail.com>
2026-08-10 17:36:16 -0500

    Add setup.py so that setuptools allows packages to use other packages.

---
+++ packages/reservoir_sampling/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="reservoir_sampling",
+    version="22",
+
+    author="kilomegagiga",
+    author_email="kilomegagiga93@gmail.com",
+
+    license="MIT",
+
+    python_requires=">=3.6.15",
+
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+)
