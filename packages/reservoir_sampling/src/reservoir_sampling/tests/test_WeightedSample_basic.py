b7c5fa21f5cd2e54381b6091daf22ecf950059be
kilomegagiga <kilomegagiga93@gmail.com>
2026-08-10 06:46:21 -0500

    Confirm minimal single item functionality of WeightedSample.

--- packages/reservoir_sampling/src/reservoir_sampling/tests/test_WeightedSample_basic.py
+++ packages/reservoir_sampling/src/reservoir_sampling/tests/test_WeightedSample_basic.py
@@ -5,3 +5,18 @@ class Test_WeightedSample:
     def test_mustInitializeWithoutError(self):
         ws = WeightedSample([],0)
         assert ws
+
+    def test_mustAcceptWeightedItem(self):
+        ws = WeightedSample([(0.1, "item")], 1)
+        assert ws
+
+    def test_mustSucceedMinimalExample(self):
+        ws = WeightedSample([(0.1, "item")], 1)
+        assert ws
+        sample = ws.extract()
+        assert sample
+        assert 1 == len(sample)
+        assert 1.0 > sample[0][0]
+        assert 0.0 < sample[0][0]
+        assert "item" == sample[0][1]
+
