95f41045d09d4424281979839f7a953eed59ecab
kilomegagiga <kilomegagiga93+git@gmail.com>
2026-08-09 22:36:10 -0500

    Confirm basic functionality of os module.

--- src/word-frequencies-bnc/tests/test_csv_basic.py
+++ src/word-frequencies-bnc/tests/test_csv_basic.py
@@ -1,4 +1,13 @@
+import os
 import csv
+
+class Test_os_module:
+
+    def test_mustFindLocationOfCurrentFile(self):
+        file_path = os.path.realpath(__file__)
+        assert file_path
+        assert '/' == file_path[:1]
+        assert '.py' == file_path[-3:]
 
 class Test_csv_module:
 
@@ -10,5 +19,5 @@ class Test_csv_module:
         reader = csv.reader(fake_file, delimiter='\t')
         result = ''
         for row in reader:
-          result = result + ', '.join(row) + '\n'
+            result = result + ', '.join(row) + '\n'
         assert ', first, second, third\n' == result
