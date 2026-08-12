c63d21b1bbf046881f100e1af9dd243be7a5f3ff
kilomegagiga <kilomegagiga93@gmail.com>
2026-08-12 01:31:30 -0500

    Create top level scripts to access TopicAssortment functionality.

--- make_grid_from_sample.py
+++ make_grid_from_sample.py
@@ -3,15 +3,15 @@ import sys
 from topic_ideas.core import TopicAssortment
 
 def main():
-    if len(sys.argv) != 2:
-        print(f"Usage: {sys.argv[0]} output.html")
+    if len(sys.argv) != 3:
+        print(f"Usage: {sys.argv[0]} input.tsv output.html")
         sys.exit(1)
 
-    output_path = sys.argv[1]
+    input_path = sys.argv[1]
+    output_path = sys.argv[2]
 
     ta = TopicAssortment()
-    ta.load_nouns()
-    ta.load_adjectives()
+    ta.read_sample(input_path)
     ta.write_html(output_path)
     sys.exit(0)
 
--- make_sample.py
+++ make_sample.py
@@ -4,7 +4,7 @@ from topic_ideas.core import TopicAssortment
 
 def main():
     if len(sys.argv) != 2:
-        print(f"Usage: {sys.argv[0]} output.html")
+        print(f"Usage: {sys.argv[0]} output.tsv")
         sys.exit(1)
 
     output_path = sys.argv[1]
@@ -12,7 +12,7 @@ def main():
     ta = TopicAssortment()
     ta.load_nouns()
     ta.load_adjectives()
-    ta.write_html(output_path)
+    ta.write_sample(output_path)
     sys.exit(0)
 
 if __name__ == "__main__":
