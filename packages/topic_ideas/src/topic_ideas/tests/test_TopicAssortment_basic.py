a0fbaf70d4250feb5c4f51a0e4ae73c3f5812a90
kilomegagiga <kilomegagiga93@gmail.com>
2026-08-12 01:04:40 -0500

    Confirm the full functionality of TopicAssortment module.

--- packages/topic_ideas/src/topic_ideas/tests/test_TopicAssortment_basic.py
+++ packages/topic_ideas/src/topic_ideas/tests/test_TopicAssortment_basic.py
@@ -8,10 +8,28 @@ from topic_ideas.core import TopicAssortment
 
 class Test_TopicAssortment:
 
-    def test_canGenerateHTML(self):
+    def test_canGenerateGrid(self):
 
         ta = TopicAssortment()
 
         ta.load_nouns()
         ta.load_adjectives()
-        ta.write_html("output.html")
+        ta.write_html("html-output.html")
+
+    def test_canGenerateSampledWords(self):
+
+        ta = TopicAssortment()
+
+        ta.load_nouns()
+        ta.load_adjectives()
+        ta.write_sample("sample-output.html")
+
+    def test_canGenerateGridFromFile(self):
+
+        ta = TopicAssortment()
+
+        ta.load_nouns()
+        ta.load_adjectives()
+        ta.write_sample("sample-output.html")
+        ta.read_sample("sample-output.html")
+        ta.write_html("html-output.html")
